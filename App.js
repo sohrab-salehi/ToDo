b30ac838e312e8f4cf17465813ede540a01c95a5
Sohrab <sohrab.4523@gmail.com>
2020-04-02 15:17:45 +0430

    Add simple alert for new todo input validation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
 import AddTodo from './components/AddTodo';
@@ -18,12 +18,18 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos,
-      ];
-    })
+    if(text.length > 3){
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos,
+        ];
+      })
+    } else {
+      Alert.alert('OOPS!', 'Todos must be over 3 cahrs long', [
+        {text: 'Undrestood'}
+      ]);
+    }
   }
 
   return (
